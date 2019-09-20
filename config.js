46fff5a5078e3116623244e18d9f3f569380535d
pBinarySoul <pBinarySoul@gmail.com>
2019-09-20 19:26:54 +0300

    Config

---
+++ config.js
@@ -0,0 +1,22 @@
+class Config
+{
+    constructor()
+    {
+        this.config = {}
+    }
+
+    getCell(key, def)
+    {
+        return this.config[key] || def;
+    }
+
+    setCell(key, value)
+    {
+        this.config[key] = value;
+    }
+}
+
+const config = new Config();
+config.setCell("StoreAddress", "http://192.168.1.215/");
+
+export default config;
