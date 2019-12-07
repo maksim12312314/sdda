49fe3c11b9b0c9759fbf7329130bc1d756f8316e
pBinarySoul <pBinarySoul@gmail.com>
2019-12-07 16:05:42 +0300

    Increased buttons size

--- components/pages/Cart/CartItem/index.js
+++ components/pages/Cart/CartItem/index.js
@@ -2,38 +2,7 @@ import React, { useContext } from "react";
 import { View, Text, TouchableOpacity, Alert } from "react-native";
 import ItemCount from "./ItemCount";
 import styles from "./styles";
-import { stateContext, dispatchContext } from "../../../../contexts";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
-const DeleteButton = (props) =>
-{
-    const { id } = props;
-    const dispatch = useContext(dispatchContext);
-
-    return (
-        <TouchableOpacity onPress={(e) =>
-        {
-            Alert.alert("УдОлить элементы", "Хотите удОлитЪ?", [
-                {
-                    text: "Отмена",
-                    style: "cancel"
-                },
-                {
-                    text: "OK",
-                    onPress: () => {
-                        dispatch({type: "DeleteFromCart", payload: id, showAlert: true});
-                        dispatch({type: "ComputeTotalPrice"});
-                    },
-                },,
-            ],
-            {cancelable: false});
-            
-        }}>
-            <FontAwesomeIcon size={16} color={"#fff"} icon={ faTimesCircle }/>
-        </TouchableOpacity>
-    )
-}
 
 /** Компонент товара в корзине */
 const CartItem = (props) =>
@@ -42,10 +11,10 @@ const CartItem = (props) =>
     return (
         <View style={styles.container}>
             <Text style={styles.item_name}>{name}</Text>
+            <Text style={styles.item_count}>{count} шт</Text>
             <View style={styles.right}>
-                <ItemCount id={id} count={count}/>
                 <Text style={styles.item_price}>{price*count}$</Text>
-                <DeleteButton id={id}/>
+                <ItemCount id={id} count={count}/>
             </View>
         </View>
     );
