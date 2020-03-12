b7645b02231fdb687abf3bde661c72ec70757647
lel <test@gmail.com>
2020-03-12 11:45:11 +0300

    3/12 fix

--- config.js
+++ config.js
@@ -27,6 +27,6 @@ class Config
 }
 
 const config = new Config();
-config.setCell("StoreAddress", "http://192.168.0.116/");
+config.setCell("StoreAddress", "http://192.168.56.1/");
 
 export default config; 
